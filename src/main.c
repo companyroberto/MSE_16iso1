8e0fea2bc0972dff6e7b4aebda9a4f5f4c4c9d03
Roberto Compañy <companyroberto@users.noreply.github.com>
2019-03-21 22:08:52 -0300

    Ejercicio 0 con delay

--- src/main.c
+++ src/main.c
@@ -37,6 +37,7 @@
 #include "main.h"
 #include "board.h"
 
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -63,15 +64,20 @@ void task_delay( uint32_t systick);
 
 void * task1( void * arg);
 void * task2( void * arg);
+void * task3( void * arg);
 
 
 /*==================[internal data definition]===============================*/
 
 uint32_t current_task = 0;
 
-uint32_t sp1, sp2;
+uint32_t sp1, sp2, sp3;
 uint32_t stack1[STACK_SIZE_B / 4];
+uint32_t task1_delay = 0;
 uint32_t stack2[STACK_SIZE_B / 4];
+uint32_t task2_delay = 0;
+uint32_t stack3[STACK_SIZE_B / 4];
+uint32_t task3_delay = 0;
 
 /*==================[external data definition]===============================*/
 
@@ -83,7 +89,6 @@ static void initHardware(void)
 	SystemCoreClockUpdate();
 	SysTick_Config(SystemCoreClock / 1000);
 }
-
 
 void task_return_hook( void * ret_val)
 {
@@ -97,63 +102,118 @@ void init_stack(uint32_t stack[], uint32_t stack_size_b, uint32_t * sp, task_typ
 
 	bzero(stack, stack_size_b);
 
-	stack[stack_size_b / 4-1] = 1 << 24;							/* xPSR.T = 1 		*/ // Posición 1
-	stack[stack_size_b / 4-2] = (uint32_t) entry_point;				/* Program Counter	*/ // Posición 2
-	stack[stack_size_b / 4-3] = (uint32_t) task_return_hook;		/* LR				*/ // Apunta a la dirección de retorno
+	stack[stack_size_b / 4 - 1] = 1 << 24;							/* xPSR.T = 1 		*/ // Posición 1
+	stack[stack_size_b / 4 - 2] = (uint32_t) entry_point;			/* Program Counter	*/ // Posición 2
+	stack[stack_size_b / 4 - 3] = (uint32_t) task_return_hook;		/* LR				*/ // Apunta a la dirección de retorno
 	// Si pongo NULL es como FreeRTOS en donde no retorna valor. Si pongo 0 da error porque no es una dirección válida ya que todas terminan en 1.
-	stack[stack_size_b / 4-8] = (uint32_t) arg;						/* R0				*/ // Para salvar el argunmento
+	stack[stack_size_b / 4 - 8] = (uint32_t) arg;					/* R0				*/ // Para salvar el argunmento
 
 	// Ahora en el systisk llamo a otra funcion entonces tengo que guardar espacio en la pila para ese Link Register
-	stack[stack_size_b / 4-9] = 0xFFFFFFF9;							/* LR IRO			*/
+	stack[stack_size_b / 4 - 9] = 0xFFFFFFF9;						/* LR IRO			*/
 
-	//*sp = (uint32_t)&(stack[stack_size_b / 4-8]);					// Es es la base de la pila, pero faltan los otros 8 registros
-	//*sp = (uint32_t)&(stack[stack_size_b / 4-16]);				// aca considero los otros 8 registros
+	//*sp = (uint32_t)&(stack[stack_size_b / 4 - 8]);				// Es es la base de la pila, pero faltan los otros 8 registros
+	//*sp = (uint32_t)&(stack[stack_size_b / 4 - 16]);				// aca considero los otros 8 registros
 
 	//
-	*sp = (uint32_t)&(stack[stack_size_b / 4-17]);					// ahora tengo que sumar uno mas al stack
+	*sp = (uint32_t) &(stack[stack_size_b / 4 - 17]);				// ahora tengo que sumar uno mas al stack por el LR IRO
 }
 
 uint32_t get_next_context(uint32_t current_sp)
 {
 	uint32_t next_sp;
 
+	// Política Round-Robin para recorrer las tareas. La 0 es el main y la 3 es la idle.
+
 	switch (current_task) {
 	case 0:
 		/* despues vere que hacer con el contexto del main 		*/
 		/* ? = current_sp										*/
-		current_task = 1;
 		next_sp = sp1;
+		current_task = 1;
 		break;
 	case 1:
 		sp1 = current_sp;
-		next_sp = sp2;
-		current_task = 2;
+
+		if ( task2_delay == 0 ){		// Si la tarea 2 no está en delay, se le pasa el puntero.
+			next_sp = sp2;
+			current_task = 2;
+		}
+		else{							// Si no paso a la idle
+			next_sp = sp3;
+			current_task = 3;
+		}
+
 		break;
 	case 2:
 		sp2 = current_sp;
-		next_sp = sp1;
-		current_task = 1;
+
+		if ( task1_delay == 0 ){		// Si la tarea 1 no está en delay, se le pasa el puntero.
+			next_sp = sp1;
+			current_task = 1;
+		}
+		else{							// Si no paso a la idle
+			next_sp = sp3;
+			current_task = 3;
+		}
+
+		break;
+	case 3:
+		sp3 = current_sp;				// Por defecto guardo para volver a esta tarea idle
+		next_sp = sp3;
+		current_task = 3;
+
+		if ( task1_delay == 0 ){		// Si la tarea 1 no está en delay, se le pasa el puntero.
+			next_sp = sp1;
+			current_task = 1;
+		}
+		else{
+			if ( task2_delay == 0 ){	// Si la tarea 2 no está en delay, se le pasa el puntero.
+				next_sp = sp2;
+				current_task = 2;
+			}
+		}
 		break;
 	default:
-		__WFI();
+		while(1){
+			__WFI();
+		}
 	}
+
+	// Si algún delay está configurado, lo decremento
+	if ( task1_delay > 0 )
+		task1_delay--;
+
+	if ( task2_delay > 0 )
+		task2_delay--;
 
 	return next_sp;
 }
 
-void task_delay( uint32_t systick)
+void task_delay( uint32_t tick)
 {
+	// En esta tarea se asigna la cantidad de tick para delay de cada tarea
 
+	switch (current_task) {
+	case 0:
+		/* no se permite la llamada a task_delay con el contexto del main */
+		__WFI();
+		break;
+	case 1:
+		task1_delay = tick;
+		break;
+	case 2:
+		task2_delay = tick;
+		break;
+	default:
+		__WFI();
+	}
+	__WFI();
 }
 
 void * task1( void * arg)
 {
 	while(1){
-		uint32_t tmp = 0;
-		for (tmp = 0; tmp < 10000000; ++tmp)	// max 4294967296
-		{
-			// esperando
-		}
+		task_delay( 1000 );
 		Board_LED_Toggle(LED_1);
 	}
 }
@@ -161,12 +221,16 @@ void * task1( void * arg)
 void * task2( void * arg)
 {
 	while(1){
-		uint32_t tmp = 0;
-		for (tmp = 0; tmp < 5000000; ++tmp)		// max 4294967296
-		{
-			// esperando
-		}
+		task_delay( 2000 );
 		Board_LED_Toggle(LED_2);
+	}
+}
+
+void * task3( void * arg)
+{
+	// Creo una tercer tarea del tipo idle la cual no debe hacer nada
+	while(1){
+		__WFI();
 	}
 }
 
@@ -177,6 +241,8 @@ int main(void)
 {
 	init_stack(stack1, STACK_SIZE_B, &sp1, task1, (void*) 0x11223344);
 	init_stack(stack2, STACK_SIZE_B, &sp2, task2, (void*) 0x55667788);
+	// Creo una tercer tarea del tipo idle la cual no debe hacer nada
+	init_stack(stack3, STACK_SIZE_B, &sp3, task3, (void*) 0x99001122);
 	// va antes de la configuracion porque si justo se dispara el systick antes de iniciaalizar las pilas es un problema...
 
 	initHardware();
